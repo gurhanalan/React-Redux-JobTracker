d1982b94f64132369482871d77a379a820221dc9
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-27 23:51:18 +0300

    Add register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  const toggleMember = () => {
+    setValues((prev) => ({ ...prev, isMember: !prev.isMember }));
+  };
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+
+        {!values.isMember && (
+          <FormRow
+            name='name'
+            type='text'
+            handleChange={handleChange}
+            value={values.name}
+          />
+        )}
+        <FormRow
+          name='email'
+          type='email'
+          handleChange={handleChange}
+          value={values.email}
+        />
+        <FormRow
+          name='password'
+          type='password'
+          handleChange={handleChange}
+          value={values.password}
+        />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
