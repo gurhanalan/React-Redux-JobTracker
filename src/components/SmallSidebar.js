7e954641c648503437c5f59e7a1b926b1f80feae
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-30 00:13:55 +0300

    Adding navlinks to smallsidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 
 import { FaTimes } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
+
 import Logo from './Logo';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
+
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -28,7 +30,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>nav links</div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>
