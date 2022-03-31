f5afab3fb90cd3356a15715d5d2f2af8c56305f7
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-03-31 23:24:12 +0300

    Add addjob content

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import FormRow from './FormRow';
 import BigSidebar from './BigSidebar';
 import NavBar from './NavBar';
 import SmallSidebar from './SmallSidebar';
+import FormRowSelect from './FormRowSelect';
 
-export { Logo, FormRow, BigSidebar, NavBar, SmallSidebar };
+export { Logo, FormRow, BigSidebar, NavBar, SmallSidebar, FormRowSelect };
